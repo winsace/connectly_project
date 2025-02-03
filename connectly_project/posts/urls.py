173c634ab1718046d95d210d67a33162e1ac28f1
winsace <123813745+winsace@users.noreply.github.com>
2025-02-03 17:03:48 +0800

    Delete connectly_project directory

--- connectly_project/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserListCreate, PostListCreate, CommentListCreate
-
-
-urlpatterns = [
-    path('users/', UserListCreate.as_view(), name='user-list-create'),
-    path('posts/', PostListCreate.as_view(), name='post-list-create'),
-    path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
-]
